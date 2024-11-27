fefcf77b89b181cbb3306b575f7c060c498563ed
Daniel Polo <od.polo@gmail.com>
2024-11-27 11:56:11 -0600

    feat: add typography variants

--- packages/mini-apps-ui-kit-react/stories/Typography.stories.tsx
+++ packages/mini-apps-ui-kit-react/stories/Typography.stories.tsx
@@ -14,141 +14,81 @@ const meta = {
         disable: true,
       },
     },
+    variant: {
+      control: "radio",
+      options: ["number", "heading", "subtitle", "body"],
+    },
   },
 } satisfies Meta<typeof Typography>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const NumberDisplay: Story = {
+export const Number: Story = {
   args: {
-    variant: "D1",
+    variant: "number",
+    level: 1,
     children: "Number Display",
   },
-};
-
-export const Number1: Story = {
-  args: {
-    variant: "N1",
-    children: "Number 1",
+  argTypes: {
+    level: {
+      control: "radio",
+      options: [1, 2, 3, 4, 5, 6],
+    },
   },
 };
 
-export const Number2: Story = {
+export const Heading: Story = {
   args: {
-    variant: "N2",
-    children: "Number 2",
+    variant: "heading",
+    level: 1,
+    children: "Heading",
+  },
+  argTypes: {
+    level: {
+      control: "radio",
+      options: [1, 2, 3, 4],
+    },
   },
 };
 
-export const Number3: Story = {
+export const Subtitle: Story = {
   args: {
-    variant: "N3",
-    children: "Number 3",
+    variant: "subtitle",
+    level: 1,
+    children: "Subtitle",
+  },
+  argTypes: {
+    level: {
+      control: "radio",
+      options: [1, 2, 3, 4],
+    },
   },
 };
 
-export const Number4: Story = {
+export const Body: Story = {
   args: {
-    variant: "N4",
-    children: "Number 4",
+    variant: "body",
+    level: 1,
+    children: "Body",
+  },
+  argTypes: {
+    level: {
+      control: "radio",
+      options: [1, 2, 3, 4],
+    },
   },
 };
-
-export const Number5: Story = {
+export const Mono: Story = {
   args: {
-    variant: "N5",
-    children: "Number 5",
+    variant: "mono",
+    level: 1,
+    children: "Mono",
   },
-};
-
-export const Number6: Story = {
-  args: {
-    variant: "N6",
-    children: "Number 6",
-  },
-};
-
-export const Heading1: Story = {
-  args: {
-    variant: "H1",
-    children: "Heading 1",
-  },
-};
-
-export const Heading2: Story = {
-  args: {
-    variant: "H2",
-    children: "Heading 2",
-  },
-};
-
-export const Heading3: Story = {
-  args: {
-    variant: "H3",
-    children: "Heading 3",
-  },
-};
-
-export const Heading4: Story = {
-  args: {
-    variant: "H4",
-    children: "Heading 4",
-  },
-};
-
-export const Subtitle1: Story = {
-  args: {
-    variant: "S1",
-    children: "Subtitle 1",
-  },
-};
-
-export const Subtitle2: Story = {
-  args: {
-    variant: "S2",
-    children: "Subtitle 2",
-  },
-};
-
-export const Subtitle3: Story = {
-  args: {
-    variant: "S3",
-    children: "Subtitle 3",
-  },
-};
-
-export const Subtitle4: Story = {
-  args: {
-    variant: "S4",
-    children: "Subtitle 4",
-  },
-};
-
-export const Body1: Story = {
-  args: {
-    variant: "B1",
-    children: "Body 1",
-  },
-};
-
-export const Body2: Story = {
-  args: {
-    variant: "B2",
-    children: "Body 2",
-  },
-};
-
-export const Body3: Story = {
-  args: {
-    variant: "B3",
-    children: "Body 3",
-  },
-};
-
-export const Body4: Story = {
-  args: {
-    variant: "B4",
-    children: "Body 4",
+  argTypes: {
+    level: {
+      control: "radio",
+      options: [1, 2, 3, 4],
+    },
   },
 };
