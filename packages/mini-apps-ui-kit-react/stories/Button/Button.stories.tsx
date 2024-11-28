20b4f6e76788e4858c50a3ea25a41cd1e1a1498c
Daniel Polo <od.polo@gmail.com>
2024-11-28 13:24:59 -0600

    fix: remove classname forwarding

--- packages/mini-apps-ui-kit-react/stories/Button/Button.stories.tsx
+++ packages/mini-apps-ui-kit-react/stories/Button/Button.stories.tsx
@@ -3,8 +3,8 @@ import { expect, fn, within } from "@storybook/test";
 import Button from "../../src/components/Button";
 import { Star } from "./Icon";
 
-const meta = {
-  title: "Button",
+const meta: Meta<typeof Button> = {
+  title: "components/Button",
   component: Button,
   parameters: {
     layout: "centered",
@@ -34,7 +34,7 @@ const meta = {
     },
   },
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
