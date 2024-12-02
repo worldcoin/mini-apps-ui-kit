5efc1cc0df5aaf18bcc0763257a3b5e9305bfd1c
Daniel Polo <od.polo@gmail.com>
2024-12-02 16:20:06 -0600

    feat: add start adornment

--- packages/mini-apps-ui-kit-react/stories/Input/Input.stories.tsx
+++ packages/mini-apps-ui-kit-react/stories/Input/Input.stories.tsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from "react";
 import type { Meta, StoryObj, StoryFn } from "@storybook/react";
 import { expect, within, userEvent } from "@storybook/test";
 import Input, { InputProps } from "../../src/components/Input";
-
+import { Star } from "../assets/Icon";
 const meta: Meta<InputProps> = {
   title: "components/Input",
   component: Input,
   parameters: {
     layout: "centered",
   },
+  argTypes: {
+    startAdornment: {
+      control: false,
+    },
+    endAdornment: {
+      control: false,
+    },
+  },
   tags: ["autodocs"],
 };
 
 export default meta;
-
 type Story = StoryObj<InputProps>;
 
 export const Text: Story = {
@@ -25,5 +32,20 @@ export const Text: Story = {
     const input = await canvas.findByPlaceholderText("Enter your name");
 
     expect(input).toBeInTheDocument();
+  },
+};
+
+export const WithStartAdornment: Story = {
+  args: {
+    placeholder: "Search",
+    startAdornment: <Star />,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = await canvas.findByPlaceholderText("Search");
+    const startAdornment = await canvas.findByTestId("input-start-adornment");
+
+    expect(input).toBeInTheDocument();
+    expect(startAdornment).toBeInTheDocument();
   },
 };
