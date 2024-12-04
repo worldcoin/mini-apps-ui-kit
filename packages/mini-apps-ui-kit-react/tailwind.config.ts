9a57c35edd68f8c3591ebd7ab46c3cf62326218d
Daniel Polo <od.polo@gmail.com>
2024-12-04 17:03:50 -0600

    feat: update tw config

--- packages/mini-apps-ui-kit-react/tailwind.config.ts
+++ packages/mini-apps-ui-kit-react/tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "tailwindcss";
 import uiKitTailwindPlugin from "./src/tailwind";
 
 const config: Config = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./stories/**/*.{html,js,ts,jsx,tsx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [uiKitTailwindPlugin],
 };
 
