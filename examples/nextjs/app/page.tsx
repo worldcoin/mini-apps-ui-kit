66d8d334b94702d2d7e563d7fab6ba6fa4ae29a0
Daniel Polo <od.polo@gmail.com>
2024-12-06 10:38:45 -0600

    fix: add use client directive and preserve in build

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -2,11 +2,10 @@ import { Button } from "@mini-apps-ui-kit/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex h-screen flex-col items-center justify-center">
       <Button>Button from @mini-apps-ui-kit/react</Button>
-      <Button>
-        Button from @mini-apps-ui-kit/react with overrided bg color from
-        uiKitTailwindPlugin
+      <Button variant="ghost">
+        Button from @mini-apps-ui-kit/react with overrided bg color from uiKitTailwindPlugin
       </Button>
     </div>
   );
