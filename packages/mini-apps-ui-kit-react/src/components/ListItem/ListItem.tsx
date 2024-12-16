6cfb8bd8fd7bd727dd8b30c41f0cf6d52c1af506
Daniel Polo <od.polo@gmail.com>
2024-12-16 11:39:20 -0600

    feat: export ListItem from components

--- packages/mini-apps-ui-kit-react/src/components/ListItem/ListItem.tsx
+++ packages/mini-apps-ui-kit-react/src/components/ListItem/ListItem.tsx
@@ -1,32 +1,38 @@
 import { cva } from "class-variance-authority";
 import { type VariantProps } from "class-variance-authority";
-import { type FC } from "react";
+import { forwardRef } from "react";
 
-const listItemStyles = cva("h-16 p-4 rounded-[14px] flex items-center text-gray-900 w-full", {
-  variants: {
-    variant: {
-      duotone: "bg-gray-50",
-      outline: "border border-gray-200",
-      ghost: "",
+const listItemStyles = cva(
+  "h-16 p-4 rounded-[0.875rem] flex items-center text-gray-900 w-full",
+  {
+    variants: {
+      variant: {
+        duotone: "bg-gray-50",
+        outline: "border border-gray-200",
+        ghost: "",
+      },
+    },
+    defaultVariants: {
+      variant: "duotone",
     },
   },
-  defaultVariants: {
-    variant: "duotone",
-  },
-});
-
+);
 interface ListItemProps
   extends VariantProps<typeof listItemStyles>,
     Omit<React.HTMLAttributes<HTMLDivElement>, "className" | "style"> {
   children?: React.ReactNode;
 }
 
-const ListItem: FC<ListItemProps> = ({ children, variant, ...props }) => {
-  return (
-    <div className={listItemStyles({ variant })} {...props}>
-      {children}
-    </div>
-  );
-};
+const ListItem = forwardRef<HTMLDivElement, ListItemProps>(
+  ({ children, variant, ...props }, ref) => {
+    return (
+      <div ref={ref} className={listItemStyles({ variant })} {...props}>
+        {children}
+      </div>
+    );
+  },
+);
+
+ListItem.displayName = "ListItem";
 
 export default ListItem;
