e86ab30801a39ef6a98a7304ed6ae994fe63e8da
Daniel Polo <od.polo@gmail.com>
2024-12-18 17:35:07 -0600

    docs: improve docs

--- packages/mini-apps-ui-kit-react/src/components/Flag/Flag.tsx
+++ packages/mini-apps-ui-kit-react/src/components/Flag/Flag.tsx
@@ -2,12 +2,36 @@ import { forwardRef } from "react";
 
 import * as FlagComponents from "./flag-components";
 
+/* Unsupported countries:
+ * - Antarctica (AQ)
+ * - Bouvet Island (BV)
+ * - Christmas Island (CX)
+ * - Western Sahara (EH)
+ * - French Guiana (GF)
+ * - South Georgia and the South Sandwich Islands (GS)
+ * - Heard Island and McDonald Islands (HM)
+ * - Saint Kitts and Nevis (KN)
+ * - Saint Martin (MF)
+ * - New Caledonia (NC)
+ * - Saint Pierre and Miquelon (PM)
+ * - Reunion (RE)
+ * - Saint Helena (SH)
+ * - Svalbard and Jan Mayen (SJ)
+ * - French Southern Territories (TF)
+ * - U.S. Minor Outlying Islands (UM)
+ * - U.S. Virgin Islands (VI)
+ * - Wallis and Futuna (WF)
+ * - Mayotte (YT)
+ */
 export type CountryCode = keyof typeof FlagComponents;
-
 interface FlagProps {
-  // ISO 3166-1 alpha-2 country code (e.g. 'US', 'GB', 'FR')
+  /**
+   * ISO 3166-1 alpha-2 country code (e.g. 'US', 'GB', 'FR')
+   */
   countryCode: CountryCode;
-  // Width and height in pixels for the flag SVG. Defaults to 40px if not specified.
+  /**
+   * Width and height in pixels for the flag SVG. Defaults to 40px if not specified.
+   */
   size?: number;
 }
 
