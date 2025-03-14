5e1444728ffbfcb5406191d1f42690e4c4b15904
Daniel Polo <od.polo@gmail.com>
2025-03-14 08:47:24 -0700

    chore: Remove Sonner dependency from mini-apps-ui-kit-react
    * Delete Sonner from package.json and pnpm-lock.yaml
    * Refactor use-toast.tsx to use React hooks directly instead of importing the entire React module

--- packages/mini-apps-ui-kit-react/src/components/Toast/use-toast.tsx
+++ packages/mini-apps-ui-kit-react/src/components/Toast/use-toast.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import { useEffect, useId, useState } from "react";
 
 import type { ToastProps } from "./Toast";
 
@@ -165,9 +165,9 @@ const toast = ({ ...props }: Toast) => {
 };
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState);
+  const [state, setState] = useState<State>(memoryState);
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState);
     return () => {
       const index = listeners.indexOf(setState);
