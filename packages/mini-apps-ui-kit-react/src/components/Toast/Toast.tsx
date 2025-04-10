b40980c92dbfc10718f62f6af455e2dc686f5981
Daniel Polo <od.polo@gmail.com>
2025-04-10 12:28:00 -0700

    fix: adjust Toast component padding for improved layout
    - Updated Toast component's viewport padding from 2 to 0.5 for better visual alignment.

--- packages/mini-apps-ui-kit-react/src/components/Toast/Toast.tsx
+++ packages/mini-apps-ui-kit-react/src/components/Toast/Toast.tsx
@@ -17,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-6 z-[100] flex max-h-screen w-full p-2 right-0 left-0 flex-col",
+      "fixed top-6 z-[100] flex max-h-screen w-full p-0.5 right-0 left-0 flex-col",
       className,
     )}
     {...props}
